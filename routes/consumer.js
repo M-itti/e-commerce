1545b537e9cfd30666b2e8378d1a7d5c4e8f1358
mehdi <mahdlinux@gmail.com>
2024-09-04 00:26:18 +0330

    minor change

---
+++ routes/consumer.js
@@ -0,0 +1,54 @@
+const { Kafka } = require('kafkajs')
+const nodemailer = require('nodemailer')
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092'],
+})
+
+const consumer = kafka.consumer({
+  groupId: 'email-service-group',
+})
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'johnjohndoe283@gmail.com',
+    pass: 'vnan pjcf llvp frnb',
+  },
+})
+
+const sendEmail = async (user) => {
+  const mailOptions = {
+    from: 'johnjohndoe283@gmail.com',
+    to: user,
+    subject: 'Welcome to Our Platform!',
+    text: `Hi ${user.name},\n\nThank you for signing up for our platform! We're excited to have you onboard.\n\nBest regards,\nThe Team`,
+  }
+
+  try {
+    await transporter.sendMail(mailOptions)
+    console.log(`Email sent to ${user}`)
+  } catch (error) {
+    console.error('Error sending email:', error)
+  }
+}
+
+const run = async () => {
+  try {
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'user-signup', fromBeginning: true })
+
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        const user = JSON.parse(message.value.toString())
+        console.log(`Received signup event for ${user}`)
+        await sendEmail(user)
+      },
+    })
+  } catch (error) {
+    console.error('Error running Kafka consumer:', error)
+  }
+}
+
+module.exports = { run, consumer }
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const process = require('process'); 
 const app = require('./app'); 
 
+<<<<<<< Updated upstream
+=======
+const PORT = process.env.PORT || 5000;
+
+>>>>>>> Stashed changes
 mongoose.connect('mongodb://127.0.0.1:27017/plants');
 
 mongoose.connection.on('connected', () => {
@@ -18,3 +23,24 @@ mongoose.connection.on('error', (err) => {
   console.error('MongoDB connection error:', err);
   process.exit(1); // Exit the process if the DB connection fails
 });
+<<<<<<< Updated upstream
+=======
+
+// Graceful shutdown handling
+const shutdown = async () => {
+  console.log('Shutting down gracefully...');
+  try {
+    await mongoose.connection.close();
+    await producer.disconnect(); 
+    console.log('Cleanup completed. Exiting...');
+    process.exit(0);
+  } catch (error) {
+    console.error('Error during shutdown:', error);
+    process.exit(1);
+  }
+};
+
+// Listen for termination signals to shutdown gracefully
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
+>>>>>>> Stashed changes
